e7a68f797c2ec0170ae54eea84db2198270d7d90
JBMagination <jbmagination@jbmagination.com>
2022-04-03 23:01:45 -0400

    Update xqcmeteor.user.js

--- xqcmeteor.user.js
+++ xqcmeteor.user.js
@@ -7,15 +7,13 @@
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         http://icons.duckduckgo.com/ip2/reddit.com.ico
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/jbmagination/xqcmeteor/main/xqcmeteor.user.js
-// @updateURL    https://raw.githubusercontent.com/jbmagination/xqcmeteor/main/xqcmeteor.user.js
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.redd.it/tf4wl0hdefr81.png";
+            i.src = "https://raw.githubusercontent.com/jbmagination/xqcmeteor/main/xqcmeteor.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
